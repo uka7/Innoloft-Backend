e4b6b2dac1135d716d6e39d700b8bd248d39e9da
uskhalid <usama.k.abdullah@gmail.com>
2023-04-13 02:43:51 +0300

    Implement AuthController for user login with User ID

---
+++ Innoloft.Web/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Innoloft.Domain.Users;
+using Innoloft.Domain.Users.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Innoloft.Web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly IUserRepository _userRepository;
+
+    public AuthController(IConfiguration configuration,
+        IUserRepository userRepository)
+    {
+        _configuration = configuration;
+        _userRepository = userRepository;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(int id)
+    {
+        var user = await _userRepository.GetUserAsync(id);
+
+        // Generate JWT token
+        var token = GenerateJwtToken(user);
+
+        return Ok(new
+        {
+            token,
+            tokenExpiration = DateTime.UtcNow.AddMinutes(_configuration.GetValue<int>("Jwt:TokenExpirationInMinutes"))
+        });
+    }
+
+    private string GenerateJwtToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Name),
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var expires = DateTime.UtcNow.AddMinutes(_configuration.GetValue<int>("Jwt:TokenExpirationInMinutes"));
+
+        var token = new JwtSecurityToken(
+            _configuration["Jwt:Issuer"],
+            _configuration["Jwt:Audience"],
+            claims,
+            expires: expires,
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
